ae173b340d34abed48123317adc7d35e5115f4c4
drake <=>
2019-03-20 10:28:38 -0400

    init

---
+++ app.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+  "fmt"
+  "net/http"
+  "net/http/httputil"
+  "log"
+  "os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    dump, err := httputil.DumpRequest(r, true)
+    if err != nil {
+      http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+      return
+    }
+
+    f, err := os.OpenFile("/var/logs.txt", os.O_APPEND|os.O_WRONLY, 0600)
+    if err != nil {
+      panic(err)
+    }
+
+    defer f.Close()
+
+    if _, err = f.Write(dump); err != nil {
+      panic(err)
+    }
+
+    w.WriteHeader(http.StatusNotFound)
+    w.Write([]byte("404 - Not Found"))
+  }
+
+func main() {
+  fmt.Println("hey there")
+  http.HandleFunc("/", handler)
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
